cf98bfca1729c559c19e2550ab4b90c3fbb4d441
aacharya00 <aacharya@hawk.iit.edu>
2025-06-05 11:34:06 -0400

    commit3

--- js/createDeck.js
+++ js/createDeck.js
@@ -11,37 +11,32 @@
 
 
   function createFlashCard(){
+    const answerContainer = document.createElement('div');
+    const answerElement= document.createElement('h5');
+    const textareaAnswer= document.createElement('textarea');
     const questionContainers = document.createElement('div');
     const questions= document.createElement('div');
     const element= document.createElement('h5');
     const textAreaQ = document.createElement('textarea');
 
 
-    const answerContainer = document.createElement('div');
-    const answerElement= document.createElement('h5');
-    const textareaAnswer= document.createElement('textarea');
-    questionContainers.classList.add('outer-container');
-
-    questions.classList.add('question-container');
     element.innerHTML='Question:';
 
-    textAreaQ.setAttribute('cols', '25');
-    textAreaQ.setAttribute('rows', '5');
+    textAreaQ.setAttribute('cols', '20');
+    textAreaQ.setAttribute('rows', '4');
     
-    textareaAnswer.setAttribute('cols', '25');
-    textareaAnswer.setAttribute('rows', '5');
-
-    answerContainer.classList.add('answer-container');
+    textareaAnswer.setAttribute('cols', '20');
+    textareaAnswer.setAttribute('rows', '4');
+    questionContainers.classList.add('outer-container');
+    questions.classList.add('question-container');
     answerElement.innerHTML='Answer:';
 
-   
+    answerContainer.appendChild(answerElement);
+    answerContainer.appendChild(textareaAnswer);
     
     questions.appendChild(element);
     questions.appendChild(textAreaQ);
     questionContainers.appendChild(questions);
-
-    answerContainer.appendChild(answerElement);
-    answerContainer.appendChild(textareaAnswer);
     questionContainers.appendChild(answerContainer);
     cards.appendChild(questionContainers);
 
